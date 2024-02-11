bb56c1f403470d2ce8b224afa1235a5029be604b
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-11 09:12:35 -0600

    cambio de Target SDK

---
+++ app/src/main/java/com/unitech/universe/UniverseActivity.kt
@@ -0,0 +1,12 @@
+package com.unitech.universe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_universe)
+    }
+}
