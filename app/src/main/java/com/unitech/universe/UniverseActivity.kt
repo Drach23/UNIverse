63465ac34f13f508c029a9e2e1194cc1c9968c99
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-11 09:16:13 -0600

    Agregando configuraciones al onCreate y poniendo un content view cn tiempo sleep en el hilo de ejecucion

--- app/src/main/java/com/unitech/universe/UniverseActivity.kt
+++ app/src/main/java/com/unitech/universe/UniverseActivity.kt
@@ -3,8 +3,9 @@ package com.unitech.universe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class UniverseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_universe)
