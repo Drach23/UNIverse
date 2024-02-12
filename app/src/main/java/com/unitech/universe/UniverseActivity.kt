a9bdfcfd4cb1ea1d6d61ee792cf3c37871139a7d
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-11 22:36:57 -0600

    Agregado de linkeo de paginas entre archivos

--- app/src/main/java/com/unitech/universe/UniverseActivity.kt
+++ app/src/main/java/com/unitech/universe/UniverseActivity.kt
@@ -1,7 +1,9 @@
 package com.unitech.universe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class UniverseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,9 @@ class UniverseActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_universe)
+    }
+    public fun goToRegister(view: View){
+        val intent = Intent(this,RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
