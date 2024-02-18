9dda5cc50cbfa7625e543462aabf476f25084293
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-17 23:10:19 -0600

    Refactor del codigo para mejor legilibilidad y agregado del Toast

--- app/src/main/java/com/unitech/universe/UniverseActivity.kt
+++ app/src/main/java/com/unitech/universe/UniverseActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class UniverseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,7 @@ class UniverseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_universe)
     }
     public fun goToRegister(view: View){
+        Toast.makeText(this,"ingresando a Registro....",Toast.LENGTH_SHORT).show()
         val intent = Intent(this,RegisterActivity::class.java)
         startActivity(intent)
     }
