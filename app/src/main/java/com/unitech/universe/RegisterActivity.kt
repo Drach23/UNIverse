47b9e634ed15d5d8631728bf341456c31548860e
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-26 22:55:01 -0600

    Agregado de comparacion de cadenas

--- app/src/main/java/com/unitech/universe/RegisterActivity.kt
+++ app/src/main/java/com/unitech/universe/RegisterActivity.kt
@@ -1,10 +1,23 @@
 package com.unitech.universe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 class RegisterActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val registerSaveButton = findViewById<Button>(R.id.registerSaveButton)
+        val firstNameEditText = findViewById<EditText>(R.id.firstNameEditText)
+
+        registerSaveButton.setOnClickListener{
+            val firstNameText = firstNameEditText.text.toString()
+            Toast.makeText(this,"Gracias $firstNameText por pulsar el boton",Toast.LENGTH_SHORT).show()
+            firstNameEditText.setText("Gracias: $firstNameText por haber pulsado el boton")
+        }
     }
 }
--- app/src/main/java/com/unitech/universe/UniverseActivity.kt
+++ app/src/main/java/com/unitech/universe/UniverseActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class UniverseActivity : AppCompatActivity() {
@@ -12,10 +14,28 @@ class UniverseActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_universe)
+
     }
     public fun goToRegister(view: View){
         Toast.makeText(this,"ingresando a Registro....",Toast.LENGTH_SHORT).show()
         val intent = Intent(this,RegisterActivity::class.java)
         startActivity(intent)
+    }
+
+    public fun login(view: View){
+        val emailEditText = findViewById<EditText>(R.id.loginEmailEditText)
+        val passwordEdiText = findViewById<EditText>(R.id.loginPasswordEditText)
+        val loginButton = findViewById<Button>(R.id.loginIngresarButton)
+
+        loginButton.setOnClickListener{
+            val email = emailEditText.text.toString()
+            val password = passwordEdiText.text.toString()
+
+            if(email.equals("universe@gmail.com") && password.equals("password123")){
+                Toast.makeText(this,"Te has logueado con exito",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"Email o contraseña invalidos",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
