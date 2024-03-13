1b42446884f4b08babaa883343d64a8b2ead0d68
Drach <88101742+Drach23@users.noreply.github.com>
2024-03-12 22:33:27 -0600

    Archivo de logica nuevo el cual incluye la funcion para agregar datos a la lista y que sea clickable

--- app/src/main/java/com/unitech/universe/ListActivity.kt
+++ app/src/main/java/com/unitech/universe/ListActivity.kt
@@ -1,4 +1,44 @@
 package com.unitech.universe
 
-class ListActivity {
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+    private  lateinit var listView: ListView
+    private lateinit var  adapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        addToList()
+
+        // Agregar el listener de clics directamente a la ListView
+        listView = findViewById(R.id.listedItemsView)
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedItem = adapter.getItem(position)
+            showMessage("Has seleccionado: $selectedItem")
+        }
+    }
+
+    fun addToList(){
+        listView = findViewById(R.id.listedItemsView)
+
+        val data = ArrayList<String>()
+        data.add("Manzana")
+        data.add("Sandia")
+        data.add("Coco")
+        data.add("Pera")
+        data.add("Jicama")
+        data.add("Mango")
+        data.add("Limon")
+
+        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,data)
+        listView.adapter = adapter
+    }
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
