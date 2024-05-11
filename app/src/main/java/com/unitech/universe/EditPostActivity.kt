caadbabc680790f7941904fe991f127264b41351
Liz Robles <robleseli9@gmail.com>
2024-05-10 22:17:16 -0600

    Creando layout para Editar Post

---
+++ app/src/main/java/com/unitech/universe/EditPostActivity.kt
@@ -0,0 +1,11 @@
+package com.unitech.universe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_post)
+    }
+}
