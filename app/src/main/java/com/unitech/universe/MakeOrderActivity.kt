3ccb815c7b7449deb5b4d7ab887b469f0e63133c
Liz Robles <robleseli9@gmail.com>
2024-05-10 22:18:09 -0600

    Creando layout para hacer pedidos

---
+++ app/src/main/java/com/unitech/universe/MakeOrderActivity.kt
@@ -0,0 +1,11 @@
+package com.unitech.universe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MakeOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_order)
+    }
+}
