e9102fb630ca2e76eb00f54f15a305a924361a51
Liz Robles <robleseli9@gmail.com>
2024-05-14 00:02:01 -0600

    Creando archivos para visualizar Pedidos

---
+++ app/src/main/java/com/unitech/universe/seller_views/OrderAdapter.kt
@@ -0,0 +1,4 @@
+package com.unitech.universe.seller_views
+
+class OrderAdapter {
+}
---
+++ app/src/main/java/com/unitech/universe/seller_views/ViewsOrdersActivity.kt
@@ -0,0 +1,12 @@
+package com.unitech.universe.seller_views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unitech.universe.R
+
+class ViewsOrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_views_orders)
+    }
+}
