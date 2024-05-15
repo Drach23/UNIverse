7caf6f04b5d5c2379407ad2361aef1beaaec99a0
Liz Robles <robleseli9@gmail.com>
2024-05-15 13:59:25 -0600

    Creando Activity para el usuarioPedidos

---
+++ app/src/main/java/com/unitech/universe/OrdersCheckUserActivity.kt
@@ -0,0 +1,11 @@
+package com.unitech.universe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrdersCheckUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders_check_user)
+    }
+}
